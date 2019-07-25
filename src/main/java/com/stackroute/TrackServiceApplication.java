908293e17e809e74241941e6620befecf5a219df
mjaheer <mjaheer1@in.ibm.com>
2019-07-25 10:15:02 +0530

    task-6-part-2

--- src/main/java/com/stackroute/TrackServiceApplication.java
+++ src/main/java/com/stackroute/TrackServiceApplication.java
@@ -1,15 +1,40 @@
 package com.stackroute;
+
+import com.stackroute.domain.Track;
+import com.stackroute.exception.TrackAlreadyExistsException;
+import com.stackroute.service.TrackService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TrackServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TrackServiceApplication.class, args);
+public class MuzixApplication extends SpringBootServletInitializer implements CommandLineRunner {
+
+	private TrackService trackService;
+
+	public MuzixApplication(TrackService trackService) {
+		this.trackService = trackService;
 	}
 
-}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MuzixApplication.class);
+	}
 
+	public static void main(String[] args) {
+		SpringApplication.run(MuzixApplication.class, args);
+	}
+
+	@Override
+	public void run(String[] args) {
+		try {
+			trackService.saveTrack(new Track(1, "Pillo talk", "Zyan"));
+			trackService.saveTrack(new Track(2, "Aaja", "Nucleya"));
+		} catch (TrackAlreadyExistsException e) {
+			e.printStackTrace();
+		}
+	}
+}
--- src/main/java/com/stackroute/config/SwaggerConfig.java
+++ src/main/java/com/stackroute/config/SwaggerConfig.java
@@ -1,27 +1,34 @@
 package com.stackroute.config;
-
+//
+import com.stackroute.muzix.domain.Track;
+import com.stackroute.muzix.exception.TrackAlreadyExistsException;
+import com.stackroute.muzix.repository.TrackRepository;
+import com.stackroute.muzix.service.TrackService;
+import org.apache.catalina.servlets.WebdavServlet;
+import org.h2.server.web.WebServlet;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import static springfox.documentation.builders.PathSelectors.regex;
-
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
-//    @Bean
-//    public Docket trackApi() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .select()
-//                .apis(RequestHandlerSelectors.basePackage("com.stackroute.muzix.controllers"))
-//                .paths(regex("/api/v1*"))
-//                .build();
-//    }
+public class WebConfiguration {
 
+    @Bean
+    ServletRegistrationBean h2servletRegistration(){
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new WebServlet());
+        servletRegistrationBean.addUrlMappings("/console/*");
+        return servletRegistrationBean;
+
+    }
 
     @Bean
     public Docket api() {
@@ -31,5 +38,6 @@ public class SwaggerConfig {
                 .paths(PathSelectors.regex("/.*"))
                 .build();
     }
+
 
 }
