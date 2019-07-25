d37b2811c18ff9b471926fb8c7eac9f136ea1d82
mjaheer <mjaheer1@in.ibm.com>
2019-07-25 08:55:08 +0530

    Lombok

--- src/main/java/com/stackroute/domain/Track.java
+++ src/main/java/com/stackroute/domain/Track.java
@@ -1,9 +1,16 @@
 package com.stackroute.domain;
-
-
 import javax.persistence.*;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Track {
 
     @Id
@@ -15,46 +22,4 @@ public class Track {
 
     @Column
     private String comment;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    public Track() {
-    }
-
-    public Track(int id, String name, String comment) {
-        this.id = id;
-        this.name = name;
-        this.comment = comment;
-    }
-
-    @Override
-    public String toString() {
-        return "Track{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", comment='" + comment + '\'' +
-                '}';
-    }
 }
