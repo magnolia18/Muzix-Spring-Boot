2240534bac2084cad4f6a3f189473e9730bc29c7
mjaheer <mjaheer1@in.ibm.com>
2019-07-25 08:59:08 +0530

    Query By Name

--- src/main/java/com/stackroute/repository/TrackRepository.java
+++ src/main/java/com/stackroute/repository/TrackRepository.java
@@ -1,10 +1,17 @@
 package com.stackroute.repository;
+
 import com.stackroute.domain.Track;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TrackRepository extends JpaRepository<Track,Integer> {
+
+    @Query("SELECT t FROM Track t WHERE t.name = ?1")
+    List<Track> getTrackbyName(String name);
 }
 //how the methods save,delete etc are working? these methods are in JPA
 //why addRepository works?
